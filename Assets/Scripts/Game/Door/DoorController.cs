dc54937f534334d74d73c6d9530d662847189375
hankyeongmoo <165135691+hankyeongmoo@users.noreply.github.com>
2025-12-28 22:35:32 +0900

    문열기

--- Assets/Scripts/Game/Door/DoorController.cs
+++ Assets/Scripts/Game/Door/DoorController.cs
@@ -12,6 +12,7 @@ public class DoorController : MonoBehaviour
 
     public void Interact()
     {
+        Debug.Log("작동");
         // 이미 문이 열려 있다면 함수를 종료 (다시 닫히지 않음)
         if (isAlreadyOpened) return;
 
@@ -19,7 +20,7 @@ public class DoorController : MonoBehaviour
         isAlreadyOpened = true; 
         animator.SetBool("isOpen", true);
         
-        // (선택 사항) 더 이상 상호작용이 필요 없다면 스크립트 자체를 비활성화할 수도 있습니다.
-        // this.enabled = false; 
+        // 더 이상 상호작용이 필요 없다면 스크립트 자체를 비활성화할 수도 있습니다.
+        this.enabled = false; 
     }
 }
