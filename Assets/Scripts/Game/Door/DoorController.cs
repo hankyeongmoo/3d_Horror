396bba9c4fee040c94d50665229b62154f199fde
hankyeongmoo <165135691+hankyeongmoo@users.noreply.github.com>
2025-12-30 19:46:34 +0900

    ..

--- Assets/Scripts/Game/Door/DoorController.cs
+++ Assets/Scripts/Game/Door/DoorController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     private Animator animator;
-    private bool isAlreadyOpened = false; // 문이 열렸는지 체크하는 변수
 
     void Start()
     {
@@ -13,11 +12,8 @@ public class DoorController : MonoBehaviour
     public void Interact()
     {
         Debug.Log("작동");
-        // 이미 문이 열려 있다면 함수를 종료 (다시 닫히지 않음)
-        if (isAlreadyOpened) return;
 
         // 문 열기 실행
-        isAlreadyOpened = true; 
         animator.SetBool("isOpen", true);
         
         // 더 이상 상호작용이 필요 없다면 스크립트 자체를 비활성화할 수도 있습니다.
